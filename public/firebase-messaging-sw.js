a8d28bbbb2458dcc4a1ed3729b668d3e111e81d8
Eugeeun <jack603@naver.com>
2025-01-18 09:18:00 +0900

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -40,3 +40,24 @@ self.addEventListener("notificationclick", event => {
   // 알림 클릭 시 특정 URL로 이동
   event.waitUntil(clients.openWindow("https://your-site-url.com"));
 });
+
+// 서비스 워커 파일
+self.addEventListener("install", function () {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function () {
+  console.log("fcm sw activate..");
+});
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
+  const notificationOptions = {
+    body: resultData.body,
+  };
+  console.log(resultData.title, {
+    body: resultData.body,
+  });
+  e.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+});
